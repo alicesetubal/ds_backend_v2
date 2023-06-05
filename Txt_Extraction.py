b73f31d656ecc2d15463cc003dba0be196d2b5bc
alicesetubal <you@example.com>
2023-06-05 19:35:02 +0100

    Iniciei código extrair txt

---
+++ Txt_Extraction.py
@@ -0,0 +1,5 @@
+#abrir e ler o aquivo txt
+with open("\\\wsl.localhost\\Ubuntu\home\ana_a_almeida\Arquivo.txt", 'r') as arquivo:
+    texto = arquivo.read()
+print(texto)
+#palavra = input("Escreva a palavra que procura:")
