94988e1466aa0c6acd9486010acee96b274bf534
alicesetubal <you@example.com>
2023-06-06 17:50:14 +0100

    Falta adicionar o xml e txt, e consertar o PDF

--- Teste_Frontend.py
+++ Teste_Frontend.py
@@ -1,5 +1,22 @@
 import streamlit as st
-
+import PyPDF2 
 st.title("Olá")
-nome = st.text_input("Qual seu nome?")
-idade = st.date_input("Que dia quer pesquisar?")
+
+arquivo= st.file_uploader("Adicione seu arquivo", type =['xml', 'pdf', ' txt'] )
+
+if arquivo:
+    print(arquivo.loads)
+    print(arquivo.read)
+    match arquivo.type:
+        case 'pdf':
+         def extrair_texto_pdf(arquivo):
+          with open(arquivo, 'rb') as pdf_file:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            num_pages = len(pdf_reader.pages)
+            texto = ''
+          for page_num in range(num_pages):
+            page = pdf_reader.pages[page_num]
+            texto += page.extract_text()
+            return texto
+else:
+    None          
